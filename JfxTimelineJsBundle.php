e47668866ab1af1934469a00084404d3691c5a90
Jérôme FIX <jerome.fix@zapoyok.info>
2013-11-02 09:09:39 +0100

    Commit initial

---
+++ JfxTimelineJsBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Jfx project.
+ *
+ * (c) Jérôme FIX <jerome.fix@zapoyok.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jfx\TimelineJsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JfxTimelineJsBundle extends Bundle
+{
+}
